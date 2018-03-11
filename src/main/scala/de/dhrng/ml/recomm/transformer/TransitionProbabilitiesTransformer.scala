3da3546efac701be904da654424610d98ef2c381
Dennis Hering <dennis.hering@nexum.de>
2018-03-11 23:54:51 +0100

    fix frequency calculation and some refactoring

--- src/main/scala/de/dhrng/ml/recomm/transformer/TransitionProbabilitiesTransformer.scala
+++ src/main/scala/de/dhrng/ml/recomm/transformer/TransitionProbabilitiesTransformer.scala
@@ -4,31 +4,61 @@ import org.apache.spark.ml.Transformer
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.mllib.fpm.AssociationRules
 import org.apache.spark.mllib.fpm.FPGrowth.FreqItemset
-import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 class TransitionProbabilitiesTransformer(sparkSession: SparkSession, minConfidence: Double) extends Transformer {
 
   override val uid: String = ""
 
-  override def transform(translogs: Dataset[_]): DataFrame = {
-    
-    val freqItemsets = translogs
-        .toDF().rdd
-        .map(row => new FreqItemset[String](
-          Array(row.getAs("antecedent"), row.getAs("consequent")), Integer.toUnsignedLong(row.getAs("frequency"))
-        ))
+  override def transform(dataset: Dataset[_]): DataFrame = {
 
-    val collect = freqItemsets.collect
+    // create FreqItemset of ((antecedent, consequent), frequency)
+    val pairFrequencies = createPairFrequencies(dataset)
 
+    // create FreqItemset of ((antecedent), frequency)
+    val antecedentFreq = createAntecedentFreq(dataset)
+
+    // union both FreqItemsets
+    val frequencies = pairFrequencies.union(antecedentFreq)
+
+    // initialize 
     val associationRules = new AssociationRules()
     associationRules.setMinConfidence(minConfidence)
 
-    // TODO: why has the result zero entries???
-    val rules = associationRules.run[String](freqItemsets)
-      .map(rule => (rule.antecedent(0), rule.consequent(0), rule.confidence))
+    val rules = associationRules.run[String](frequencies)
+      .distinct()
+      .map(rule => Row(rule.antecedent(0), rule.consequent(0), rule.confidence))
 
-    sparkSession.createDataFrame(rules)//.select("antecedent", "consequent", "confidence")
+    sparkSession.createDataFrame(rules, transformSchema(dataset.schema))
+  }
+
+  def createPairFrequencies(dataset: Dataset[_]) = {
+
+    val antecedentCol = dataset.schema.fields(0).name;
+    val consequentCol = dataset.schema.fields(1).name;
+    val frequencyCol = dataset.schema.fields(2).name;
+
+
+    dataset.toDF().rdd
+      .map(row =>
+        new FreqItemset[String](
+          Array(row.getAs(antecedentCol), row.getAs(consequentCol)),
+          Integer.toUnsignedLong(row.getAs(frequencyCol))
+        ))
+  }
+
+  def createAntecedentFreq(dataset: Dataset[_]) = {
+
+    val antecedentCol = dataset.schema.fields(0).name;
+    val frequencyCol = dataset.schema.fields(2).name;
+
+    dataset.toDF().rdd
+      .map(row => {
+        (row.getAs(antecedentCol), Integer.toUnsignedLong(row.getAs(frequencyCol)))
+      }: (String, Long))
+      .reduceByKey((a, b) => a + b)
+      .map(row => new FreqItemset[String](Array(row._1), row._2))
   }
 
   override def copy(extra: ParamMap): Transformer = {
@@ -37,8 +67,8 @@ class TransitionProbabilitiesTransformer(sparkSession: SparkSession, minConfiden
 
   override def transformSchema(schema: StructType): StructType = {
     StructType(
-      StructField("antecedent", StringType, false) ::
-        StructField("consequent", StringType, false) ::
+      schema.fields(0) ::
+        schema.fields(1) ::
         StructField("confidence", DoubleType, false) :: Nil)
   }
 
