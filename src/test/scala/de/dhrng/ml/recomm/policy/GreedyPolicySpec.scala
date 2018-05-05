b3a9654f6861f4ded5aa836fdca56cbc112b4ffb
dhering <hering.dennis@googlemail.com>
2018-05-05 15:57:58 +0200

    add and user ActionValue model

--- src/test/scala/de/dhrng/ml/recomm/policy/GreedyPolicySpec.scala
+++ src/test/scala/de/dhrng/ml/recomm/policy/GreedyPolicySpec.scala
@@ -1,6 +1,6 @@
 package de.dhrng.ml.recomm.policy
 
-import de.dhrng.ml.recomm.model.ml.StateProbability
+import de.dhrng.ml.recomm.model.ml.{ActionValue, StateProbability}
 import org.scalatest._
 
 class GreedyPolicySpec extends FeatureSpec with GivenWhenThen with Matchers with
@@ -11,16 +11,16 @@ class GreedyPolicySpec extends FeatureSpec with GivenWhenThen with Matchers with
     scenario("select greedy") {
 
       Given("a set of probabilities")
-      def probabilities = Seq(
-        StateProbability("A", 0.2), StateProbability("B", 0.7), StateProbability("A", 0.1)
+      def actionValues = Seq(
+        ActionValue("A", 0.2), ActionValue("B", 0.7), ActionValue("A", 0.1)
       )
 
       When("the policy selects a probability")
-      val probability = GreedyPolicy.select(probabilities)
+      val probability = GreedyPolicy.select(actionValues)
 
       Then("the highest probability is chosen")
       assert(probability.state == "B")
-      assert(probability.probability == 0.7D)
+      assert(probability.value == 0.7D)
     }
   }
 }
